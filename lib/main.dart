b1e67bf0cc1d325a18fa67c9dbd262514801cf7f
Joaquin <neo.jcc@gmail.com>
2021-09-26 11:19:41 -0600

    ¿Buscas la bandera?

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter CTF Flag',
       theme: ThemeData(
           primarySwatch: Colors.blue,
           inputDecorationTheme: InputDecorationTheme(
